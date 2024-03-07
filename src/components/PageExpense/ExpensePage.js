cb16080d14ecde8ae6a380e72d0348277d2bc8cf
TCTrung <trungtrinh1325@gmail.com>
2024-03-08 01:31:33 +0700

    update form expense

--- src/components/PageExpense/ExpensePage.js
+++ src/components/PageExpense/ExpensePage.js
@@ -1,6 +1,6 @@
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import {Tabs, TabList, TabPanels, Tab, TabPanel, FormLabel, FormControl, Input, useDisclosure} from '@chakra-ui/react';
 import axios from "axios";
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Doughnut} from 'react-chartjs-3';
 import {
     Table,
@@ -12,10 +12,19 @@ import {
     Td,
     TableCaption,
     TableContainer,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+} from '@chakra-ui/react';
+import { FaPen } from "react-icons/fa";
 import { Image } from '@chakra-ui/react';
 import { CgCalendarDates } from "react-icons/cg";
-import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { CiCalendarDate } from "react-icons/ci";
@@ -25,8 +34,12 @@ import { LiaCalendarWeekSolid } from "react-icons/lia";
 import {MDBCard, MDBCardBody} from "mdb-react-ui-kit";
 import "./Expense.css"
 import TransactionService from "../../services/transactions.services";
-const ExpensePage = ({wallet_id}) => {
-
+import {ListItem} from "@material-tailwind/react";
+import {useWallet} from "../WalletContext";
+const IncomePiechart = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -36,32 +49,34 @@ const ExpensePage = ({wallet_id}) => {
     console.log(userdata)
 
     const [listTransaction, setListTransaction] = useState([]);
-    const [exCategory, setExCategory] = useState([])
-    const [exAmount, setExAmount] = useState([])
+    const [incomeCategory, setIncomeCategory] = useState([])
+    const [incomeAmount, setIncomeAmount] = useState([])
     const [currentMonthIndex, setCurrentMonthIndex] = useState(new Date().getMonth());
     const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+    const [wallet_id, setWallet_id] = useState('all');
+
+    const { selectedWalletId } = useWallet();
 
     useEffect(() => {
-        if (wallet_id) {
-            const fetchData = async () => {
-                getTransactionEx(userdata, wallet_id);
-            };
-            fetchData();
-        }
-    }, [wallet_id]);
+        const fetchData = async () => {
+            getTransactionIncome(userdata, selectedWalletId);
+        };
+        fetchData();
+    }, [selectedWalletId, currentYear, currentMonthIndex]);
 
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     const handlePrevNextMonths = (currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, increment) => {
-        TransactionService.handlePrevNextMonths(currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, increment)
+        TransactionService.handlePrevNextMonths(currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, increment);
+
     };
 
     const handleCurrentMonth = (setCurrentMonthIndex, setCurrentYear) => {
         TransactionService.handleCurrentMonth(setCurrentMonthIndex, setCurrentYear);
     };
-    const getTransactionEx = (userdata, wallet_id) => {
+    const getTransactionIncome = (userdata, wallet_id) => {
         if (wallet_id) {
-            axios.get(`http://localhost:8080/api/transactions/user/${userdata.id}/expense_transaction/${wallet_id}`)
+            axios.get(`http://localhost:8080/api/transactions/user/${userdata.id}/expense_transaction/${wallet_id}/date/${currentYear}/${currentMonthIndex}`)
                 .then((res) => {
                     console.log(res);
                     setListTransaction(res.data);
@@ -74,41 +89,37 @@ const ExpensePage = ({wallet_id}) => {
         }
     };
     function getlist(transactions) {
-        const exCategory = [];
-        const exAmount = [];
+        const incomeCategory = [];
+        const incomeAmount = [];
         transactions.forEach(transaction => {
-            // nếu transaction thuộc type expense
+            // nếu transaction thuộc type income
             if (transaction.category.type === "EXPENSE") {
-                const index = exCategory.indexOf(transaction.category.name)
+                const index = incomeCategory.indexOf(transaction.category.name)
                 if (index === -1) {
-                    exCategory.push(transaction.category.name);
-                    exAmount.push(transaction.amount);
+                    incomeCategory.push(transaction.category.name);
+                    incomeAmount.push(transaction.amount)
                 }
-                exAmount[index] += transaction.amount
+                incomeAmount[index] += transaction.amount
             }
         })
-        setExCategory(exCategory);
-        setExAmount(exAmount)
+        setIncomeCategory(incomeCategory);
+        setIncomeAmount(incomeAmount)
     }
     const data = {
-        labels: exCategory,
+        labels: incomeCategory,
         datasets: [{
-            data: exAmount,
+            data: incomeAmount,
             backgroundColor: [
                 '#FF6384',
                 '#36A2EB',
                 '#FFCE56',
                 '#9aff56',
-                '#e056ff',
-                '#5664ff'
             ],
             hoverBackgroundColor: [
                 '#FF6384',
                 '#36A2EB',
                 '#FFCE56',
                 '#a2ff56',
-                '#9456ff',
-                '#56ffeb'
             ]
         }]
     };
@@ -117,61 +128,79 @@ const ExpensePage = ({wallet_id}) => {
             <MDBCard className="card-chart">
                 <MDBCardBody >
                     <div style={{width: '50%',height: '50%',margin: 'auto'}}>
+                        {startDate && endDate && (
+                            <div>
+                                <p>From: {startDate}</p>
+                                <p>To: {endDate}</p>
+                            </div>
+                        )}
+                        <div className="flex justify-content-center mt-0.5">
 
-                        {/*<div className="flex justify-content-center mt-0.5">*/}
-
-                        {/*    <Button*/}
-                        {/*        variant="text"*/}
-                        {/*        className="rounded-none border-b border-blue-gray-50 bg-transparent p-2 btn-month"*/}
-                        {/*        indicatorProps={{*/}
-                        {/*            className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",*/}
-                        {/*        }}*/}
-                        {/*        onClick={() => handlePrevNextMonths(currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, -1)}>*/}
-                        {/*        {currentMonthIndex === 0 ? months[11] : months[currentMonthIndex - 1]} {currentMonthIndex === 0 ? currentYear - 1 : currentYear}*/}
-                        {/*    </Button>*/}
-                        {/*    <Button variant="text"*/}
-                        {/*            className="rounded-none border-b border-blue-gray-50 bg-transparent p-2 btn-month btn-color"*/}
-                        {/*            indicatorProps={{*/}
-                        {/*                className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",*/}
-                        {/*            }}>{months[currentMonthIndex]} {currentYear}</Button>*/}
-                        {/*    <Button*/}
-                        {/*        variant="text"*/}
-                        {/*        className="rounded-none border-b border-blue-gray-50 bg-transparent p-2 btn-month"*/}
-                        {/*        indicatorProps={{*/}
-                        {/*            className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",*/}
-                        {/*        }}*/}
-                        {/*        onClick={() => handlePrevNextMonths(currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, 1)}>*/}
-                        {/*        {currentMonthIndex === 11 ? months[0] : months[currentMonthIndex + 1]} {currentMonthIndex === 11 ? currentYear + 1 : currentYear}*/}
-                        {/*    </Button>*/}
-                        {/*</div>*/}
+                            <Button
+                                variant="text"
+                                className="rounded-none border-b border-blue-gray-50 bg-transparent p-2 btn-month"
+                                indicatorProps={{
+                                    className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
+                                }}
+                                onClick={() => handlePrevNextMonths(currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, -1)}>
+                                {currentMonthIndex === 0 ? months[11] : months[currentMonthIndex - 1]} {currentMonthIndex === 0 ? currentYear - 1 : currentYear}
+                            </Button>
+                            <Button variant="text"
+                                    className="rounded-none border-b border-blue-gray-50 bg-transparent p-2 btn-month btn-color"
+                                    indicatorProps={{
+                                        className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
+                                    }}>{months[currentMonthIndex]} {currentYear}</Button>
+                            <Button
+                                variant="text"
+                                className="rounded-none border-b border-blue-gray-50 bg-transparent p-2 btn-month"
+                                indicatorProps={{
+                                    className: "bg-transparent border-b-2 border-gray-900 shadow-none rounded-none",
+                                }}
+                                onClick={() => handlePrevNextMonths(currentMonthIndex, setCurrentMonthIndex, currentYear, setCurrentYear, 1)}>
+                                {currentMonthIndex === 11 ? months[0] : months[currentMonthIndex + 1]} {currentMonthIndex === 11 ? currentYear + 1 : currentYear}
+                            </Button>
+                        </div>
                         <Tabs isFitted variant='enclosed'>
                             <CgCalendarDates style={{width: '30px',height: '30px',marginLeft: '100%'}} onClick={handleShow} />
                             <TabPanels>
                                 <TabPanel>
-                                    <Doughnut data={data} />
-                                    <div style={{maxHeight: '300px',margin: 'auto',overflowY: 'auto'}}>
+                                    {listTransaction.length === 0 ? (
+                                        <div style={{ height: "430px" }}>
+                                            <ListItem>
+                                                No transactions for this month
+                                            </ListItem>
+                                            <Button variant="outlined" onClick={() => handleCurrentMonth(setCurrentMonthIndex, setCurrentYear)}>Back to Current Month</Button>
+                                        </div>
+                                    ) : (
+                                        <div>
+                                            <Doughnut  data={data} />
+                                            <div style={{maxHeight: '300px',margin: 'auto',overflowY: 'auto'}}>
 
-                                        <TableContainer>
-                                            <Table variant='simple'>
-                                                <Tbody>
-                                                    {listTransaction.map((transaction) => (
-                                                        <Tr>
-                                                            <Td style={{ display: 'flex', alignItems: 'center' }}>
-                                                                <Image
-                                                                    borderRadius='full'
-                                                                    boxSize='50px'
-                                                                    src={transaction.category.image}
-                                                                    alt=""
-                                                                />
-                                                                <span style={{ marginLeft: '5px' }}>{transaction.category.name}</span>
-                                                            </Td>
-                                                            <Td style={{ textAlign: 'right' }}>{transaction.amount} vnd </Td>
-                                                        </Tr>
-                                                    ))}
-                                                </Tbody>
-                                            </Table>
-                                        </TableContainer>
-                                    </div>
+                                                <TableContainer>
+                                                    <Table variant='simple'>
+                                                        <Tbody>
+                                                            {listTransaction.map((transaction) => (
+                                                                <Tr>
+                                                                    <Td style={{ display: 'flex', alignItems: 'center' }}>
+                                                                        <Image
+                                                                            borderRadius='full'
+                                                                            boxSize='50px'
+                                                                            src={transaction.category.image}
+                                                                            alt=""
+                                                                        />
+                                                                        <span style={{ marginLeft: '5px' }}>{transaction.category.name}</span>
+                                                                    </Td>
+                                                                    <Td style={{ textAlign: 'right' }}>{transaction.amount} vnd</Td>
+                                                                </Tr>
+                                                            ))}
+                                                        </Tbody>
+                                                    </Table>
+                                                </TableContainer>
+                                            </div>
+                                        </div>
+                                    )
+                                    }
+
                                 </TabPanel>
                                 <TabPanel>
                                     <p>two!</p>
@@ -199,12 +228,44 @@ const ExpensePage = ({wallet_id}) => {
                             <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}>
                                 <LiaCalendarWeekSolid style={{ height: '40px', width: '40px' }} /> <span style={{ marginLeft: '5px' }}>Year</span>
                             </div>
+                            <div style={{ display: 'flex', alignItems: 'center', marginTop: '20px' }} onClick={onOpen}>
+                                <FaPen style={{ height: '20px', width: '20px',marginLeft: '10px' }} /> <span style={{ marginLeft: '15px' }}>Custom</span>
+                            </div>
                         </Offcanvas.Body>
                     </Offcanvas>
                 </MDBCardBody>
             </MDBCard>
 
+            {/*modal custom*/}
+            <Modal
+                isOpen={isOpen}
+                onClose={onClose}
+            >
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Select Date Range</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6}>
+                        <FormControl>
+                            <FormLabel>From</FormLabel>
+                            <Input type={"date"} value={startDate} onChange={(e) => setStartDate(e.target.value)} />
+                        </FormControl>
+
+                        <FormControl mt={4}>
+                            <FormLabel>To</FormLabel>
+                            <Input type={"date"} value={endDate} onChange={(e) => setEndDate(e.target.value)} />
+                        </FormControl>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button onClick={onClose} colorScheme='green' variant='outline'>Cancel</Button>
+                        <Button colorScheme='greengreen' variant='outline'>
+                            Select Time
+                        </Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+
         </div>
     );
 };
-export default ExpensePage;
+export default IncomePiechart;
